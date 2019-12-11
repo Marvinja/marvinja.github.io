6ce01889a4343d1833bdfde28cce309a5fb7e6c6
Marvin Agbuya <marvinjohnagbuya@gmail.com>
2019-12-11 16:29:12 +1100

    Update wave_brush.js

--- wave-brush/wave_brush.js
+++ wave-brush/wave_brush.js
@@ -7,25 +7,27 @@ function setup() {
   //initialise object
   orb = new Orb(random(width), random(height));
   orb2 = new Orb(random(width), random(height));
-
 }
 
 
 function draw() {
-  if (second() % 10 == 0) {
-  fill(0, 10);
+    blendMode(NORMAL);
+  if (second() % 20 == 0) {
+  fill(0, 20);
   noStroke();
-  rect(0, 0, width, height);
+  rect(-width/2, 0, width*2, height);
   }
+  translate(width/2, 0);
   orb.display();
   orb.update();
   orb2.display();
   orb2.update();
-
+  blendMode(ADD);
   strokeWeight(2);
-  stroke(255, 20);
+  stroke(0, 100, 200, 30);
   if (dist(orb.position.x, orb.position.y, orb2.position.x, orb2.position.y)) {
     line(orb.position.x, orb.position.y, orb2.position.x, orb2.position.y);
+    line(-orb.position.x, orb.position.y, -orb2.position.x, orb2.position.y);
   }
 }
 
@@ -33,8 +35,8 @@ class Orb {
   constructor(_x, _y) {
     this.position = createVector(_x, _y);
     this.easing = random(0.01, 0.06);
-    this.targetPosition = createVector(random(-width/4, width+ width/4), random(-height/4, height + height/4));
-    this.targetPosition2 = createVector(random(-width/4, width+ width/4), random(-height/4, height + height/4));
+    this.targetPosition = createVector(random(-width/2, width/2), random(-height/4, height + height/4));
+    this.targetPosition2 = createVector(random(-width/2, width/2), random(-height/4, height + height/4));
   }
 
   display() {
@@ -49,7 +51,7 @@ class Orb {
     this.targetPosition.x += (this.targetPosition2.x - this.targetPosition.x) * this.easing;
     this.targetPosition.y += (this.targetPosition2.y - this.targetPosition.y) * this.easing;
     if (dist(this.targetPosition.x, this.targetPosition.y, this.targetPosition2.x, this.targetPosition2.y) < 100) {
-      this.targetPosition2 = createVector(random(-width/4, width+ width/4), random(-height/4, height + height/4));
+      this.targetPosition2 = createVector(random(-width/2, width/2), random(-height/4, height + height/4));
     }
   }
 }
